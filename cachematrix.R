f5c5832f306573fa3c1f6602958b3e4580da52c6
tonnsg <tantnt1991@gmail.com>
2014-05-22 21:48:05 +0100

    For coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <-function(y){
+    x<<- y
+    I<<- NULL
+  }
+  get <- function()x
+  setInverse <- function(Inverse) I <<-Inverse
+  getInverse <- function() I
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
+## This function creates a list containing a function to:
+## (1)Set the value of the matrix
+## (2)Get the value of the matrix
+## (3)Set the value of the inverse matrix
+## (4)Get the value of the inverse matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...){
+    I <- x$getInverse()
+    if(!is.null(I)){
+      message("getting cached data")
+      return(I)
+    }
+    data <-x$get()
+    I <- solve(data,...)
+    x$setInverse(I)
+    I
+  }
+  
+##This function first attempts to get the inverse of the matrix. By
+## using 'getInverse', if I is not NULL, it will return the message 
+## getting cached data and return the value of I stored.
+## However, if it is NULL, the inverse of the matrix will be 
+## calculated, by first getting the value of the matrix, and using 
+## 'solve' to solve for the inverse. It will then store the result
+## into 'I' using the setInverse function. 
